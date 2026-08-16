a49f9e36b123adfa061c149def1fc30fe80ce7c7
Moulali <shaikmouin2002@gmail.com>
2026-08-16 15:26:39 +0530

    Improve QR authentication UI

--- qr_auth/settings.py
+++ qr_auth/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static",]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 # Email
